35f792d5edaeaf9d75aff7a338f6c9ad3af887b2
Leandro Silva Farias <leandrosilvafarias@MacBook-Air-de-Leandro.local>
2025-08-31 14:59:43 -0300

    Rota autenticacao coach

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from contextlib import asynccontextmanager
 
 from passlib.context import CryptContext
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 
 import models
-from sqlmodel import SQLModel, Session
+from models import Coach
+from sqlmodel import SQLModel, Session, select
 
 from DbManager import engine
+from auth import create_access_token, verify_password, get_password_hash
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -33,24 +37,47 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
+def get_session() -> Session:
+    with Session(engine) as session:
+        yield session
+
+
 @app.post("/coach/", status_code=status.HTTP_201_CREATED)
-async def create_coach(coach: models.CoachCreate):
-    hashed_password = pwd_context.hash(coach.password)
+async def create_coach(coach: models.CoachCreate,
+                       session: Session = Depends(get_session)):
+    hashed_password = get_password_hash(coach.password)
 
     db_coach = models.Coach(
         **coach.dict(exclude={"password"}),
         password_hash=hashed_password
-
     )
 
-    with Session(engine) as session:
-        try:
-            session.add(db_coach)
-            session.commit()
-            session.refresh(db_coach)
-            response = db_coach.dict(exclude={"password_hash"})
-            return response
-        except Exception as e:
-            session.rollback()
-            print(f"Error creating coach: {e}")
-            raise HTTPException(status_code=400, detail=f"Failed to create coach: {e}")
+    try:
+        session.add(db_coach)
+        session.commit()
+        session.refresh(db_coach)
+        response = db_coach.dict(exclude={"password_hash"})
+        return response
+    except Exception as e:
+        session.rollback()
+        print(f"Error creating coach: {e}")
+        raise HTTPException(status_code=400, detail=f"Failed to create coach: {e}")
+
+
+@app.post("/coach/login/")
+async def perform_login_coach(form_data: OAuth2PasswordRequestForm = Depends(),
+                              session: Session = Depends(get_session)):
+    username = form_data.username
+    password = form_data.password
+
+    coach: Coach | None = session.exec(
+            select(Coach)
+            .where(Coach.user_name == username)
+        ).first()
+    print('Coach -> ', coach)
+    if coach and verify_password(password, coach.password_hash):
+        access_token = create_access_token(data={"sub": str(coach.id)})
+        return {
+            "access_token": access_token,
+            "coach_id": str(coach.id)
+        }
