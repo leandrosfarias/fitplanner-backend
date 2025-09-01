8d6f28a87bc16e802b27e2aa4a106f9fa4c3800f
Leandro Silva Farias <leandrosilvafarias@MacBook-Air-de-Leandro.local>
2025-09-01 11:45:43 -0300

    feat: Implements get_coach_by_id

--- DbManager.py
+++ DbManager.py
@@ -1,5 +1,6 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, Session, select
 import os
+import models
 
 
 database_url = os.getenv("DATABASE_URL")
@@ -7,3 +8,11 @@ if not database_url:
     raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(database_url)
+
+
+async def get_coach_by_id(coach_id: str):
+    with Session(engine) as session:
+        result = session.exec(
+            select(models.Coach)
+            .where(models.Coach.id == coach_id))
+        return result.first()
